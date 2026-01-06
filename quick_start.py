53dfe8bb45ec5a5a04ccde75ee284cfb23294f45
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2026-01-06 18:24:43 -0300

    fiz: ajuste caminho importe respeitando hierarquia de pastas

--- quick_start.py
+++ quick_start.py
@@ -10,8 +10,8 @@ from pathlib import Path
 from src.core.genetic_algorithm import calculate_distance
 
 # Importações dos novos módulos (quando criar os arquivos)
-# from models import Entrega, Veiculo, Base, PrioridadeEntrega
-# from medical_genetic_algorithm import calculate_fitness_with_constraints, generate_priority_biased_population
+# from src.models import Entrega, Veiculo, Base, PrioridadeEntrega
+# from src.core.medical_ga import calculate_fitness_with_constraints, generate_priority_biased_population
 
 
 def criar_dados_exemplo():
